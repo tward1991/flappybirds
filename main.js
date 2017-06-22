88209bc0c75beb9b0721a0723bf108d0bb1c1f71
tward1991 <w1616543@student.southstaffs.ac.uk>
2017-06-22 10:19:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -101,10 +101,11 @@ hitPipe: function()  {
   game.state.start('main');
 // Initialize Phaser, and create a 400px by 490px game
 var game = new Phaser.Game(400, 490);
+  },
 
 // Add the 'mainState' and call it 'main'
 game.state.add('main', mainState);
 
 // Start the state to actually start the game
 game.state.start('main');
-  };
+  
